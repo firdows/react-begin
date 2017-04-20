0bb20ed0c6a7321dc86bcb0a74e2f2efc258e614
Ahamad <ahamad.jedu@gmail.com>
2017-04-20 10:33:56 +0700

    First install react,jspm

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React ,{ Component }from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld';
+
+
+ReactDOM.render(<HelloWorld/>,document.getElementById('app'));
+
+//console.log('ok');
