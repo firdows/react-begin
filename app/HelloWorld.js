6038c9ce2c0917df051d5f9818de26223f439e8f
Firdows <ahamad.jedu@gmail.com>
2017-04-20 03:58:03 +0000

    Insert img

--- app/HelloWorld.js
+++ app/HelloWorld.js
@@ -4,7 +4,12 @@ import React ,{ Component } from 'react';
 export default class HelloWorld extends Component{
     render(){
         return (
+            <p>
             <h4>Hello World .js</h4>
+           
+            <img width="100" src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.0-9/14520415_1154409071313211_8205465538680337409_n.jpg?oh=1f5f69774bb8de87546343c451fddeff&oe=594CCEF9" />
+               สวัสดีครับพี่
+             </p>
         );
     }
 }
